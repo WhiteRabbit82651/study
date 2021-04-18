7f12538169b57443a47614e73d5208025ced27df
lobo <lobo6217@gmail.com>
2021-04-18 17:58:24 +0900

    main:add new C# project

---
+++ visual_studio_2019/C3/MsgClient/MsgClient/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace MsgClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Program pg = new Program();
+            pg.print("start program");
+
+            // サーバーのIPアドレスを受け取る
+            Console.Write("Enter the IP address of the server:");
+            string ServerIpAddress = Console.ReadLine();
+
+            // サーバーのポート番号を受け取る
+            Console.Write("Enter the server port number:");
+            string ServerPortNo = Console.ReadLine();
+
+            //サーバーにメッセージを送信する
+            pg.SendMsg("Hello Socket communication!", ServerIpAddress, int.Parse(ServerPortNo));
+            pg.print("end program");
+            Console.ReadLine();
+        }
+        private void SendMsg(string msg, String ServerIpAddress, int PortNo)
+        {
+            System.Net.Sockets.TcpClient tcp = new System.Net.Sockets.TcpClient(ServerIpAddress, PortNo);
+            print("サーバー({0}:{1})と接続しました({2}:{3})",
+                ((System.Net.IPEndPoint)tcp.Client.RemoteEndPoint).Address,
+                ((System.Net.IPEndPoint)tcp.Client.RemoteEndPoint).Port,
+                ((System.Net.IPEndPoint)tcp.Client.LocalEndPoint).Address,
+                ((System.Net.IPEndPoint)tcp.Client.LocalEndPoint).Port);
+
+            // Streamを取得する
+            System.Net.Sockets.NetworkStream ns = tcp.GetStream();
+
+            // タイムアウトを設定する
+            ns.ReadTimeout = 10000;
+            ns.WriteTimeout = 10000;
+
+            // エンコードを指定する
+            System.Text.Encoding enc = System.Text.Encoding.UTF8;
+
+            // 送信する文字列をbyte配列に変換する
+            byte[] sendBytes = enc.GetBytes(msg + '\n');
+
+            // 文字列をサーバーに送信する
+            ns.Write(sendBytes, 0, sendBytes.Length);
+
+            // サーバーから文字列を取得する
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            byte[] resBytes = new byte[256];
+            int resSize = 0;
+
+            do
+            {
+                resSize = ns.Read(resBytes, 0, resBytes.Length);
+                if (resSize == 0)
+                {
+                    print("サーバーが切断されました");
+                    break;
+                }
+
+                ms.Write(resBytes, 0, resSize);
+
+            } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');
+
+            // サーバーから受け取ったByte文字列を文字列に変換する
+            string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+
+            // Streamを切断する
+            ms.Close();
+
+            // サーバーから受け取った文字列を表示する
+            resMsg = resMsg.TrimEnd('\n');
+            print(resMsg);
+
+            ns.Close();
+            tcp.Close();
+            print("切断しました");
+        }
+        private void print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        private void print(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+    }
+}
---
+++ visual_studio_2019/C3/MsgServer/MsgServer/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace MsgServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            pg.print("start server");
+            Console.Write("Enter the server port number:");
+            String PortNo = Console.ReadLine();
+            pg.server(int.Parse(PortNo));
+            pg.print("end server");
+            Console.ReadLine();
+        }
+        private void server(int PortNo)
+        {
+            // サーバー用IPAddresを作成
+            System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse("127.0.0.1");
+
+            // ソケット通信を開始する
+            System.Net.Sockets.TcpListener listener = new System.Net.Sockets.TcpListener(ipAdd, PortNo);
+            listener.Start();
+            System.Net.IPEndPoint LocalEndPoint = (System.Net.IPEndPoint)listener.LocalEndpoint;
+            pring("Listenを開始しました({0}:{1})。", LocalEndPoint.Address, LocalEndPoint.Port);
+
+            // 接続要求があったら受け入れる
+            System.Net.Sockets.TcpClient client = listener.AcceptTcpClient();
+            System.Net.IPEndPoint RemoteEndPoint = (System.Net.IPEndPoint)client.Client.RemoteEndPoint;
+            pring("クライアント({0}:{1})と接続しました。", RemoteEndPoint.Address, RemoteEndPoint.Port);
+
+            // NetworkStreamを取得データの流れ
+            System.Net.Sockets.NetworkStream ns = client.GetStream();
+            ns.ReadTimeout = 10000;
+            ns.WriteTimeout = 10000;
+
+            System.Text.Encoding enc = System.Text.Encoding.UTF8;
+            bool disconnected = false;
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+            byte[] resBytes = new byte[256];
+            int resSize = 0;
+
+            do
+            {
+                //データの一部を受信する
+                resSize = ns.Read(resBytes, 0, resBytes.Length);
+                //Readが0を返した時はクライアントが切断したと判断
+                if (resSize == 0)
+                {
+                    disconnected = true;
+                    print("クライアントが切断しました。");
+                    break;
+                }
+                //受信したデータを蓄積する
+                ms.Write(resBytes, 0, resSize);
+                //まだ読み取れるデータがあるか、データの最後が\nでない時は、
+                // 受信を続ける
+
+            } while (ns.DataAvailable || resBytes[resSize - 1] != '\n');
+
+            //受信したデータを文字列に変換
+            string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+            ms.Close();
+
+            resMsg = resMsg.TrimEnd('\n');
+            print(resMsg);
+
+            if (!disconnected)
+            {
+                //クライアントにデータを送信する
+                //クライアントに送信する文字列を作成
+                string sendMsg = resMsg.Length.ToString();
+
+                //文字列をByte型配列に変換
+                byte[] sendBytes = enc.GetBytes(sendMsg + '\n');
+
+                //データを送信する
+                ns.Write(sendBytes, 0, sendBytes.Length);
+                print(sendMsg);
+            }
+
+            ns.Close();
+            client.Close();
+            print("クライアントとの接続を閉じました。");
+
+            listener.Stop();
+            print("Listenerを閉じました。");
+        }
+        private void print(String msg)
+        {
+            Console.WriteLine(msg);
+        }
+        private void pring(string format, params object[] arg)
+        {
+            Console.WriteLine(format, arg);
+        }
+    }
+}
